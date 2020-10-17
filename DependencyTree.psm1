9566757e393899f2b464ac60e0e7524b79e0c161
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2020-10-17 23:09:17 +0100

    Revert "Scratch - messing with PS classes"
    This reverts commit 73a41a61b99c65f21cafba7539e8befaa3b8ffe0.

--- DependencyTree.psm1
+++ DependencyTree.psm1
@@ -1,30 +1,3 @@
 
-# Testing state of play with PS classes
-# classes not exported;
-#   $host.ExitNestedPrompt(); $Module = ipmo .\DependencyTree.psd1 -Force -PassThru; & $Module {$host.EnterNestedPrompt()}
-#
-# https://github.com/poshbotio/PoshBot => build process puts classes in the .psm1
-#
-class Dependency
-{
-    hidden [Dependency]$RequiredBy
-    hidden [Collections.Generic.IList[Dependency]]$Requires = [Collections.Generic.List[Dependency]]::new()
-
-    [void] AddRequirement([Dependency]$Dep)
-    {
-        [void]$this.Requires.Add($Dep)
-    }
-
-    [string] ToString()
-    {
-        return "bar"
-    }
-}
-
-class Visitor
-{
-
-}
-
 Get-ChildItem $PSScriptRoot\Private\*.ps1 | ForEach-Object {. $_.FullName}
 Get-ChildItem $PSScriptRoot\Public\*.ps1  | ForEach-Object {. $_.FullName}
