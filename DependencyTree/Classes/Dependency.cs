bbd749028a924cd82acae71a6107e977177c79a5
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2020-10-18 00:05:23 +0100

    Start on Dependency class

---
+++ DependencyTree/Classes/Dependency.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Management.Automation;
+using System.Collections.Generic;
+
+namespace DependencyTree
+{
+    public class Dependency
+    {
+        public Dependency (string name) => this.name = name;
+        public Dependency (string name, Version requiredVersion) : this(name) => this.requiredVersion = requiredVersion;
+        public Dependency (string name, Version minimumVersion, Version maximumVersion) : this(name)
+        {
+            if (minimumVersion == maximumVersion)
+            {
+                this.requiredVersion = minimumVersion;
+            }
+            else
+            {
+                this.minimumVersion = minimumVersion;
+                this.maximumVersion = maximumVersion;
+            }
+        }
+
+        private Dependency requiredBy;
+        private List<Dependency> requires;
+        private string name;
+
+        private Version resolvedVersion;    // Version of the discovered object that satisfies the requirement
+        private Version minimumVersion;
+        private Version maximumVersion;
+        private Version requiredVersion;
+
+        private object satisfiedBy;
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
