a06e32faab1dacbcfa00359abb73c73159d2d217
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2020-10-18 00:58:40 +0100

    Fun with nullables

--- DependencyTree/Classes/Dependency.cs
+++ DependencyTree/Classes/Dependency.cs
@@ -6,9 +6,13 @@ namespace DependencyTree
 {
     public class Dependency
     {
-        public Dependency (string name) => this.name = name;
-        public Dependency (string name, Version requiredVersion) : this(name) => this.requiredVersion = requiredVersion;
-        public Dependency (string name, Version minimumVersion, Version maximumVersion) : this(name)
+        public Dependency(string name)
+        {
+            this.name = name;
+            this.requires = new List<Dependency>();
+        }
+        public Dependency(string name, Version requiredVersion) : this(name) => this.requiredVersion = requiredVersion;
+        public Dependency(string name, Version? minimumVersion, Version? maximumVersion) : this(name)
         {
             if (minimumVersion == maximumVersion)
             {
@@ -21,28 +25,26 @@ namespace DependencyTree
             }
         }
 
-        private Dependency requiredBy;
-        private List<Dependency> requires;
+        private Dependency? requiredBy;
+        private IList<Dependency> requires;
         private string name;
 
-        private Version resolvedVersion;    // Version of the discovered object that satisfies the requirement
-        private Version minimumVersion;
-        private Version maximumVersion;
-        private Version requiredVersion;
+        private Version? resolvedVersion;    // Version of the discovered object that satisfies the requirement
+        private Version? minimumVersion;
+        private Version? maximumVersion;
+        private Version? requiredVersion;
 
-        private object satisfiedBy;
+        private object? satisfiedBy;
 
-        public string VersionString()
-        {
-            return this switch
+        public string VersionString() =>
+            this switch
             {
-                {requiredVersion: not null}                             => requiredVersion.ToString(),
-                {minimumVersion: not null, maximumVersion: not null}    => $"{minimumVersion}-{maximumVersion}",
-                {maximumVersion: not null}                              => $"<={maximumVersion}",
-                {minimumVersion: not null}                              => $">={minimumVersion}",
-                _                                                       => "*"
+                { requiredVersion: not null } => requiredVersion.ToString(),
+                { minimumVersion: not null, maximumVersion: not null } => $"{minimumVersion}-{maximumVersion}",
+                { maximumVersion: not null } => $"<={maximumVersion}",
+                { minimumVersion: not null } => $">={minimumVersion}",
+                _ => "*"
             };
-        }
 
         public override string ToString()
         {
