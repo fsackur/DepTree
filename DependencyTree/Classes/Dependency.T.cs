f2e658e2e4e265335afc317ae65a6f151d4485af
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2020-10-18 18:53:05 +0100

    Add generic Dependency

---
+++ DependencyTree/Classes/Dependency.T.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Management.Automation;
+using System.Collections.Generic;
+
+namespace DependencyTree
+{
+    public class Dependency<T> : Dependency
+    {
+        public T? ResolvedBy;
+        public bool IsResolved { get => ResolvedBy != null; }
+
+
+        public Dependency(string name, VersionConstraint versionConstraint) : base(name, versionConstraint) { }
+
+
+        public void ResolveWith(T dependencyObject)
+        {
+            if (dependencyObject is null) throw new ArgumentNullException(nameof(dependencyObject));
+
+            ResolvedBy = dependencyObject;
+        }
+
+
+        public void AddDependency(Dependency dependency)
+        {
+            if (dependency is null) throw new ArgumentNullException(nameof(dependency));
+
+            if (!IsResolved) throw new InvalidOperationException("Cannot add dependencies of an unresolved dependency.");
+
+            Requires.Add(dependency);
+        }
+    }
+}
